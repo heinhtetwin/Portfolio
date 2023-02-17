b2c288498205b7fb997dd301ae4567ac8b4b2b3d
heinhtet.win <heinhtet.win@frontiir.net>
2023-02-17 15:00:07 +0630

    changed blog domain

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -90,7 +90,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://www.heinhtetwin.com"
+                href="https://blog.heinhtetwin.com"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -100,7 +100,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/heinhtetwin/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
