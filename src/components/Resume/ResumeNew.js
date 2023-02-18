b4970f4b41fdae27dddf7532f27c92afb3975bc5
Hein Htet Win <48096774+heinhtetwin@users.noreply.github.com>
2023-02-18 20:20:03 +0630

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Hein Htet Win - resume.pdf";
+import pdf from "../../Assets/../Assets/Hein Htet Win - Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://drive.google.com/file/d/1K8aofBGpJxaxgBhMFLFUfe8Xnufj-GC1/view?usp=sharin";
+  "https://drive.google.com/file/d/1rJHdnF3uG8iHBr__hXLSNDfqHp0b_HJ_/view?usp=sharing";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
