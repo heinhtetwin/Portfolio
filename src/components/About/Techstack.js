a3a17931dc69df77334f39284681190f5fbdcac5
Hein Htet Win <heinhtetwin386@gmail.com>
2023-02-21 21:45:53 +0630

    Optimized

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  DiJavascript1,
-  DiReact,
   DiNodejs,
   DiMongodb,
   DiPython,
@@ -15,9 +13,6 @@ import {
   SiKubernetes,
   SiAnsible,
   SiTerraform,
-  SiPytorch,
-  SiFirebase,
-  SiNextdotjs,
 } from "react-icons/si";
 
 function Techstack() {
